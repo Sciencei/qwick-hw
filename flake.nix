f4f7ff8dc4827a4ebccaf851fe36806001574d2c
Cullin Poresky <cporeskydev@gmail.com>
2024-04-01 16:21:18 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.default = {
+
+          packages = {
+          };
+
+          devShell = {
+            tools = hp: {
+              ghcid = pkgs.ghcid;
+            };
+            hlsCheck.enable = false;
+          };
+
+          autoWire = [ "packages" "apps" "checks" ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "my-haskell-package custom development shell";
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+          ];
+          nativeBuildInputs = with pkgs; [
+          ];
+        };
+      };
+    };
+}
